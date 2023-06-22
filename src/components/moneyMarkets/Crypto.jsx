5ff4cc63ccb21e8546a037ad9bffd1d928860485
ETH1Elohim <ezmax.defi@gmail.com>
2023-06-22 15:29:59 -0500

    support matic

--- src/components/moneyMarkets/Crypto.jsx
+++ src/components/moneyMarkets/Crypto.jsx
@@ -13,7 +13,7 @@ export const Crypto = (props) => {
         <button className='bg-transparent text-lg hover:bg-gray-100 p-4 rounded-lg hover:scale-110 ease-in duration-300 w-full flex items-center justify-start mx-2 shadow-md font-semibold' id='BNB' onClick={() => props.onSelectSymbol('BNB')}><img src="/assets/BNB.png" alt='Binance' className='w-10 rounded-full mr-3' />Binance</button>
     </div>
     <div className='w-full h-auto m-auto rounded-full flex items-center justify-center p-4'>
-        <button className='bg-transparent text-lg hover:bg-gray-100 p-4 rounded-lg hover:scale-110 ease-in duration-300 w-full flex items-center justify-start mx-2 shadow-md font-semibold' id='LINK' onClick={() => props.onSelectSymbol('LINK')} ><img src="/assets/chainlink.png" alt='Chainlink' className='w-10 rounded-full mr-3' />Chainlink</button>
+        <button className='bg-transparent text-lg hover:bg-gray-100 p-4 rounded-lg hover:scale-110 ease-in duration-300 w-full flex items-center justify-start mx-2 shadow-md font-semibold' id='MATIC' onClick={() => props.onSelectSymbol('MATIC')} ><img src="/assets/matic.png" alt='MATIC' className='w-10 rounded-full mr-3' />Polygon</button>
     </div>
     </>
   )
