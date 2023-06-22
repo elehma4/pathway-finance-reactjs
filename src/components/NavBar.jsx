fdd0cb36085e060e910885574ca50971f3e5a5ae
ETH1Elohim <ezmax.defi@gmail.com>
2023-06-22 10:52:43 -0500

    mobile navbar working

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,6 @@ const NavBar = () => {
 
     const [nav, setNav] = useState(false)
     const [shadow, setShadow] = useState(false)
-    
 
     const handleNav = () => {
         setNav(!nav)
@@ -26,6 +25,7 @@ const NavBar = () => {
 
     return (
         <div className={shadow ? 'fixed w-full h-20 bg-[#212121] shadow-xl z-[100]' : 'fixed w-full h-20 bg-[#212121] z-[100]'}>
+          {/* Computer Nav: */}
             <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
                 <Link to='main' smooth={true} duration={500} className=' cursor-pointer'>
                     <img src="/assets/path-logo-white1.png" alt="/" width='147' height='37' />
@@ -51,6 +51,45 @@ const NavBar = () => {
                     </div>
                 </div>
             </div>
+            {/* End Computer Nav */}
+
+          {/* Mobile Nav: */}
+          <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70 text-white' : ''}>
+            <div className={nav ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#212121] p-10 ease-in duration-500' : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'}>
+              <div>
+                <div className='flex w-full items-center justify-between'>
+                  <img src="/assets/path-logo-white1.png" alt="/" width='129' height='37' />
+                  <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+                    <AiOutlineClose />
+                  </div>
+                </div>
+                <div className='border-b border-gray-300 my-4'>
+                  <p className='w-[85%] md:w-[90%] py-4'>Your Financial Compass.</p>
+                </div>
+              </div>
+              <div className='py-4 flex flex-col'>
+                <ul className='uppercase'>
+                  <Link onClick={()=> setNav(false)} to='main' smooth={true} duration={500}>
+                  <li className='py-4 text-base'>Home</li>
+                  </Link>
+                  <Link onClick={()=> setNav(false)} to='markets' smooth={true} duration={500}>
+                  <li className='py-4 text-base'>Markets</li>
+                  </Link>
+                  <Link onClick={()=> setNav(false)} to='news' smooth={true} duration={500}>
+                  <li className='py-4 text-base'>News</li>
+                  </Link>
+                </ul>
+                <div className='pb-10 fixed bottom-0'>
+                  <p className='uppercase tracking-widest'>
+                    Built for pioneers.
+                  </p>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* End Mobile Nav */}
+
         </div>
       )
 }
